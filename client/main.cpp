2a683014db97a55770e555b270edfff84bcd1baf
arash <arashsameniy@gmail.com>
2021-06-21 16:47:07 +0430

    client connected to database

--- client/main.cpp
+++ client/main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
-
+#include <QSqlDatabase>
+#include <QMessageBox>
 #include <QApplication>
+
+bool initDatabase()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("../database/HayDayDB.sqlite");
+    return db.open();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    if(!initDatabase())
+    {
+        QMessageBox::critical(nullptr, "Error", "Failed to connect to database.");
+        exit(0);
+    }
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
