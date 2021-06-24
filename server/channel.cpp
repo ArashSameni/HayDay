2042140e14276cb74508890c35e5db837119d29f
arash <arashsameniy@gmail.com>
2021-06-24 23:50:16 +0430

    Login method connected to database

--- server/channel.cpp
+++ server/channel.cpp
@@ -3,22 +3,29 @@
 
 int Channel::login(const QString& username, const QString& password)
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", QString::number(socketDescriptor));
+    db.setDatabaseName("../database/HayDayDB.sqlite");
+    db.open();
     QSqlQuery query;
     query.prepare("SELECT id FROM Accounts WHERE username=:username AND password=:password");
     query.bindValue(":username", username);
     query.bindValue(":password", password);
     query.exec();
 
-    if (query.next())
+    if (query.first())
     {
+        int account_id = query.value(0).toInt();
         query.clear();
         query.prepare("SELECT id FROM Farmers WHERE account_id=:account_id");
-        query.bindValue(":account_id", query.value(0).toInt());
+        query.bindValue(":account_id", account_id);
         query.exec();
-        query.next();
+        query.first();
+
+        db.close();
         return query.value(0).toInt();
     }
 
+    db.close();
     return 0;
 }
 
@@ -50,7 +57,8 @@ void Channel::readyRead()
         data.remove("Login(");
         QString username = data.split(',')[0];
         QString password = data.split(',')[1];
-        password.truncate(password.size() - 1);
+        password.chop(1);
+        qDebug() << username << password;
 
         data.setNum(login(username, password));
         socket->write(data.toUtf8());
--- server/main.cpp
+++ server/main.cpp
@@ -2,22 +2,9 @@
 #include <QSqlDatabase>
 #include "tcpserver.h"
 
-bool initDatabase()
-{
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("../database/HayDayDB.sqlite");
-    return db.open();
-}
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    if(!initDatabase())
-    {
-        qDebug() << "Error: Failed to connect to database.";
-        exit(EXIT_FAILURE);
-    }
 
     TcpServer Server;
     Server.startServer();
