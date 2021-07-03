f4a4ecd25f8bb1fbb714faf8d5d44a6d5edc249e
arash <arashsameniy@gmail.com>
2021-07-03 04:44:04 +0430

    Change username to lowercase

--- client/auth.cpp
+++ client/auth.cpp
@@ -5,14 +5,14 @@
 
 int Auth::login(const QString &username, const QString &password)
 {
-    socket.write("Login(" + username + "," + password + ")");
+    socket.write("Login(" + username.toLower() + "," + password + ")");
     int account_id = socket.read().toInt();
     return account_id;
 }
 
 int Auth::signup(const QString &username, const QString &password)
 {
-    socket.write("Signup(" + username + "," + password + ")");
+    socket.write("Signup(" + username.toLower() + "," + password + ")");
     int account_id = socket.read().toInt();
     return account_id;
 }
